79fa41488d1eb85d20078e0db0603adcc7ebe550
joaoezx <joao.a.freitas0405@gmail.com>
2024-04-30 20:55:08 -0300

    Express lê JSON

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // imports packages
 
 require("dotenv").config()
-const express = require("express")
-const postgres = require("postgres")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
 
-const app = express()
-
+// Config JSON response
+app.use(express.json())
 
 // Public Route
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Bem vindo!"})
-})
+});
 
-app.listen(3000)
+app.listen(3000);
