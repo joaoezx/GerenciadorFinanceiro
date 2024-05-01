bfebf906bf0eb6f14b56f47ce6265bd1bd283090
joaoezx <joao.a.freitas0405@gmail.com>
2024-04-30 23:16:18 -0300

    Cadastrar o usuario e validar o nome

--- app.js
+++ app.js
@@ -14,4 +14,16 @@ app.get("/", (req, res) => {
     res.status(200).json({ msg: "Bem vindo!"})
 });
 
+//Register User
+app.post("/auth/register", async(req, res) => {
+
+    const {name, email, income ,password, confirmpassword} = req.body
+
+    // validations
+    if(!name) {
+        return res.status(422).json({ msg: "O nome é obrigatorio!"})
+    }
+
+});
+
 app.listen(3000);
