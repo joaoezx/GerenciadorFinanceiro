88d1b48ca6d83ae687e0168b4b79f7437b2b314b
joaoezx <joao.a.freitas0405@gmail.com>
2024-05-01 00:48:49 -0300

    validação de dados do usuário

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 // Config JSON response
 app.use(express.json())
 
+// Models
+const User = require("./models/User")
+
 // Public Route
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Bem vindo!"})
@@ -17,11 +20,23 @@ app.get("/", (req, res) => {
 //Register User
 app.post("/auth/register", async(req, res) => {
 
-    const {name, email, income ,password, confirmpassword} = req.body
+    const {name, email, password, confirmpassword, income} = req.body;
 
     // validations
     if(!name) {
-        return res.status(422).json({ msg: "O nome é obrigatorio!"})
+        return res.status(422).json({ msg: "O nome é obrigatorio!"});
+    }
+    if(!email) {
+        return res.status(422).json({ msg: "O email é obrigatorio!"});
+    }
+    if(!password) {
+        return res.status(422).json({ msg: "A senha é obrigatoria!"});
+    }
+    if(password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senha precisam ser iguais!"});
+    }
+    if(income < 0) {
+        return res.status(422).json({ msg: "A renda é obrigatoria!"});
     }
 
 });
